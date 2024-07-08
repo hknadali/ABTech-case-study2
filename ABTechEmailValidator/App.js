e433b219793415f3145e9d742861f47e53621bf0
Hakan Adali <hakan17adali@gmail.com>
2024-07-08 16:20:07 +0300

    Updated App.js: email validator application implemented

--- ABTechEmailValidator/App.js
+++ ABTechEmailValidator/App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, SafeAreaView, Button, TextInput } from 'react-native';
+import validateEmail from 'abtech-email-validation';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [isValid, setIsValid] = useState(null);
+
+  const handleValidate = () => {
+    setIsValid(validateEmail(email));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput 
+        style={styles.input}
+        placeholder='Enter your email'
+        value={email}
+        onChangeText={setEmail}
+        keyboardType='email-address'
+      />
+      <Button title='Check Email' onPress={handleValidate} />
+      {isValid !== null && (
+        <Text style={[styles.result, isValid ? styles.valid : styles.invalid]}>
+          {isValid ? 'Valid Email' : 'Invalid Email'}
+        </Text>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffe4c4',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderColor: '#000',
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+  },
+  result: {
+    marginTop: 16,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  valid: {
+    color: 'green',
+  },
+  invalid: {
+    color: 'red',
   },
 });
